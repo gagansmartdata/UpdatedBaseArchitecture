66cf4ed0fa0cfbeb8047ce3add5b96352346923f
Gagandeep Singh <gagandeep.singh@smartdatainc.net>
2021-10-21 17:33:06 +0530

    java class converted to kotlin

--- app/src/main/java/com/ggn/updatedbasearchitecture/base/LoadingViewHolder.kt
+++ app/src/main/java/com/ggn/updatedbasearchitecture/base/LoadingViewHolder.kt
@@ -1,12 +1,8 @@
-package com.ggn.updatedbasearchitecture.base;
+package com.ggn.updatedbasearchitecture.base
 
-import android.view.View;
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 
-import androidx.recyclerview.widget.RecyclerView;
-
-
-class LoadingViewHolder extends RecyclerView.ViewHolder {
-    public LoadingViewHolder(View itemView) {
-        super(itemView);
-    }
-}
+internal class LoadingViewHolder(itemView: View?) : RecyclerView.ViewHolder(
+    itemView!!
+)
